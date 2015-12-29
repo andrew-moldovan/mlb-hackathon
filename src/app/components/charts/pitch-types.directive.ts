c6ef76a9acf511fcb8abbb64f2d21c7b9ccdc315
Andrew Moldovan <anmoldov@microsoft.com>
2015-12-28 20:31:36 -0800

    created radial gradients for pitch types and positive outcomes

--- src/app/components/charts/pitch-types.directive.ts
+++ src/app/components/charts/pitch-types.directive.ts
@@ -17,27 +17,27 @@ module mlbHackathon {
       link: (scope: any, ele: JQuery, attrs: ng.IAttributes) => {
         scope.sp.d3Service.d3().then(d3 => {
           console.log(scope.sp.data);
-
-          var data = [];
-          for (var key in scope.sp.data.pitchTypes) {
-            data.push({
-              pitchType: key,
-              value: scope.sp.data.pitchTypes[key],
-              radius: (Math.sqrt(scope.sp.data.pitchTypes[key].totalPitches / Math.PI)*7)
-            })
-          };
-          console.log(data);
           var width = ele.parent().width() - scope.sp.margin.left - scope.sp.margin.right;
-
           var svg = d3.select(ele[0]).append("svg")
               .attr("width", width + scope.sp.margin.left + scope.sp.margin.right)
               .attr("height", scope.sp.height + scope.sp.margin.top + scope.sp.margin.bottom)
             .append("g")
               .attr("transform", "translate(" + scope.sp.margin.left + "," + scope.sp.margin.top + ")");
-
           scope.$on('$destroy', () => {
             scope.sp.d3HelperService.removeSVGOnDestroy(svg, d3);
           });
+
+          var data = [];
+          for (var key in scope.sp.data.pitchTypes) {
+            // let's get rid of the really tiny ones that don't matter
+            if (scope.sp.data.pitchTypes[key].totalPitches > 10) {
+              data.push({
+                pitchType: key,
+                value: scope.sp.data.pitchTypes[key],
+                radius: (Math.sqrt(scope.sp.data.pitchTypes[key].totalPitches / Math.PI)*7)
+              })
+            }
+          };
 
           var circles = svg.selectAll("circle")
             .data(data, function(d) {
@@ -45,6 +45,26 @@ module mlbHackathon {
             })
             .enter()
             .append("circle");
+
+          var grads = svg.append("defs").selectAll("radialGradient")
+              .data(data)
+            .enter()
+              .append("radialGradient")
+              .attr("gradientUnits", "objectBoundingBox")
+              .attr("cx", 0)
+              .attr("cy", 0)
+              .attr("r", "100%")
+              .attr("id", function(d, i) { return "grad" + i; });
+
+          grads.append("stop")
+              .attr("offset", function(d) {
+                return ((d.value.positive / d.value.totalPitches) * 100) + "%";
+              })
+              .style("stop-color", "rgb(233,237,230)");
+
+          grads.append("stop")
+              .attr("offset", "50%")
+              .style("stop-color", "rgb(122,158,110)");
 
           var largestRadius = d3.max(data, function(d) { return d.radius; });
           var xLine = 10;
@@ -69,22 +89,52 @@ module mlbHackathon {
                   xLine2 = xLine2 + lastRadius + 40 + d.radius;
                 }
                 lastRadius = d.radius;
+                d.centerX = xLine2;
                 return xLine2;
               }
               lastRadius = d.radius;
+              d.centerX = xLine;
               return xLine;
             })
             .attr("cy", function (d, i) {
               if (d.secondLine) {
+                d.centerY = largestRadius * 3;
                 return largestRadius * 3;
               }
+              d.centerY = largestRadius;
               return largestRadius;
             })
             .attr("r", function (d) {
               return d.radius;
             })
-            .style("fill", "steelblue" );
+            .style("fill", function(d, i) {
+              console.log(d, i);
+              return "url(#grad" + i + ")";
+            });
 
+            svg.selectAll("text")
+              .data(data, function(d) {
+                return d.pitchType;
+              })
+              .enter()
+              .append("text")
+              .attr("x", function(d) {
+                return d.centerX - 7;
+              })
+              .attr("y", function(d) {
+                return d.centerY + 5;
+              })
+              .attr("fill", "white")
+              .text(function(d) {
+                return d.pitchType;
+              });
+
+              svg.append("text")
+                .attr("x", (width / 2))
+                .attr("y", -20)
+                .attr("text-anchor", "middle")
+                .style("font-size", "2rem")
+                .text(scope.sp.title);
         });
       },
       bindToController: true
